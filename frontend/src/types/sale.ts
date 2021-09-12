082a54757bce80dcc738fe1b83d2ed2670005deb
Marcio Sousa <mjs__sousa@hotmail.com>
2021-09-12 16:45:32 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,6 +1,29 @@
+import { Seller } from "./seller";
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;    
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean; 
+}
+
 export type saleSum = {
-    sellerName: String,
-    sum: number
+    sellerName: String;
+    sum: number;
 }
 
 export type SaleSuccess = {
